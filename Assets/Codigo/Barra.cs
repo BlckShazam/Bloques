b5bead8942dbc0e4a8b5adb6f942da31f28c313d
BlckShazam <Ylevendnc@gmail.com>
2017-01-11 13:05:48 +0100

    Inicio
    En realidad es la continuacion del proyecto para poder usarlo donde
    tenga GitHub

---
+++ Assets/Codigo/Barra.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class Barra : MonoBehaviour {
+    public float velocidad = 0.4f;
+    Vector3 posicionInicial;
+
+	// Use this for initialization
+	void Start () {
+        posicionInicial = transform.position;
+       
+	
+	}
+    void Reset()
+    {
+        transform.position = posicionInicial;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float tecladoHorizontal = Input.GetAxisRaw ("Horizontal");
+        float posX = transform.position.x + (tecladoHorizontal * velocidad * Time.deltaTime); // Time.deltaTime calcula el tiempo en funcion de los fotogramas para ajustar la velocidad
+        transform.position = new Vector3(Mathf.Clamp(posX, -8, 8), transform.position.y, transform.position.z); // Clamp define los maximos y minimos que puede tener un vector, 1� el valor actual, minimo y maximo.
+        
+	
+	}
+}
---
+++ Assets/Codigo/Pelota.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using System.Collections;
+
+public class Pelota : MonoBehaviour {
+
+    public float velocidadInicial = 600f;
+
+    public Rigidbody rig;
+
+    bool enJuego = false;
+
+    Vector3 posicionInicial;
+
+    Transform barra;
+
+
+    private void Awake()
+    {
+        barra = transform.parent; 
+        //para obtener otro Transform de otro padre: barra = transform.parent.GetComponent<NombreDelComponenteAObtenerSuReferencia>();
+
+    }
+    // Use this for initialization
+
+    void Start () {
+        posicionInicial = transform.position;
+	
+	}
+    public void Reset()
+    {
+        transform.position = posicionInicial;
+        transform.SetParent(barra);
+        enJuego = false;
+        rig.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (!enJuego && Input.GetButtonDown("Fire1")) {
+            enJuego = true;
+            transform.SetParent(null);
+            rig.isKinematic = false;
+            rig.AddForce(new Vector3(velocidadInicial, velocidadInicial, 0));
+        }
+	
+	}
+}
