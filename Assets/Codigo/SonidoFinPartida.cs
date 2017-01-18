b0b940f0a4f60f090882d3c623688bb7ecaf8225
BlckShazam <Ylevendnc@gmail.com>
2017-01-18 13:45:10 +0100

    Trabajando- Sonidos y Musica
    Se están integrando los sonidos de las acciones en el juego junto con la
    música de las escenas.

---
+++ Assets/Codigo/SonidoFinPartida.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidoFinPartida : MonoBehaviour {
+
+	public AudioSource audioSource;
+	public AudioClip completado;
+	public AudioClip gameOver;
+
+	public void GameOver(){
+	
+		audioSource.clip = gameOver;
+
+	}
+
+		
+
+}
---
+++ Assets/Codigo/SonidosPelota.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SonidosPelota : MonoBehaviour {
+
+	public AudioSource rebotes;
+	public AudioSource puntos;
+
+
+	void OnCollisionEnter (Collision otro){
+	
+
+		//Comparamos el Tag del objeto para decidir si el audio que se escucha es el de puntos o el de rebotes.
+		if (otro.gameObject.CompareTag ("Bloques")) {
+			puntos.Play ();
+		
+		} else {
+		
+			rebotes.Play ();
+		
+		}
+	
+	}
+}
